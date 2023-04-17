d1190a33996ef964b02a90e4a80e86b7c7302087
zabbix-byte <zabbix@ztrunk.space>
2023-04-17 20:52:35 +0200

    Adding base

---
+++ Server/ServerMain.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace lenguaje.Server
+{
+    public class ServerMain : BaseScript
+    {
+        public ServerMain()
+        {
+        }
+    }
+}
